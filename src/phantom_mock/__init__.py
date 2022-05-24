1189647dc1e55f3338095f2a68ee779d0c67284e
Daniel Federschmidt <dfederschmidt@splunk.com>
2022-05-24 16:00:54 +0200

    some import fixes

--- src/phantom_mock/__init__.py
+++
@@ -1,4 +0,0 @@
-def vault_info(vault_id):
-    sucess, message, vault_info = ""
-
-    return sucess, message, vault_info
--- src/pytest_splunk_soar_connectors/__init__.py
+++ src/pytest_splunk_soar_connectors/__init__.py
@@ -6,10 +6,10 @@ import phantom_mock.phantom
 import phantom_mock.phantom.app
 import phantom_mock.phantom.vault
 import phantom_mock.phantom.action_result
-import phantom_mock.phantom.base_connector
+from phantom_mock.phantom import base_connector
 
 sys.modules['phantom'] = phantom_mock.phantom
 sys.modules['phantom.app'] = phantom_mock.phantom.app
 sys.modules['phantom.vault'] = phantom_mock.phantom.vault
 sys.modules['phantom.action_result'] = phantom_mock.phantom.action_result
-sys.modules['phantom.base_connector'] = phantom_mock.phantom.base_connector
+sys.modules['phantom.base_connector'] = base_connector
